dea73332daf82030f920b77711f204d694f165a7
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2023-05-20 21:43:34 -0300

    Update main.c

--- trab/main.c
+++ trab/main.c
@@ -39,7 +39,7 @@ void rgraph(FILE *input, int n, char matriz[n][n]) {
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
             fscanf(input, "%c", &matriz[i][j]);
-            if ( i==j && matriz[i][j] == 'F') {
+            if (i==j && matriz[i][j] == 'F') {
                 printf("INFELIZMENTE OS C(K)AIOS ESTAVAM BEBADOS\n");
                 exit(1);
             }
@@ -49,8 +49,7 @@ void rgraph(FILE *input, int n, char matriz[n][n]) {
 }
 
 int n_familias(int n, char matriz[n][n], int *visitados) {
-    int n_familias=0;
-    int i;
+    int n_familias=0, i;
     for (i=0; i<n; i++)
         visitados[i]=-1;
     for (i=0; i<n; i++)
@@ -69,8 +68,7 @@ void n_familias2(int n, char matriz[n][n], int *visitados, int vertice, int n_fa
 }
 
 void conta_graus(int n, char matriz[n][n], int *graus) {
-    int i, j;
-    int contador;
+    int i, j, contador;
     for (i=0; i<n; i++) {
         contador=0;
         for(j=0; j<n; j++)
@@ -92,13 +90,13 @@ void conta_vertices(int n, int *visitados, int numero_familias, int *total) {
 }
 
 void verifica (int n, int numero_familias, int *total_familias, int *graus, int *visitados) {
-    int grau_max, completos=true, i,j;
+    int grau_max,completos=true,i,j;
     for(i=1; i<=numero_familias; i++) {
         if (!completos)
             break;
         grau_max = total_familias[i]-1;
         for (j=0; j<n; j++)
-            if ( visitados[j] == i )
+            if (visitados[j] == i)
                 if ( graus[j] != grau_max ) {
                     completos = false;
                     break;
