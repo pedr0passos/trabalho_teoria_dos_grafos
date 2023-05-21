c987fb3bfcd26970edc1b59da577b739a8b8f2a8
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2023-05-20 21:32:03 -0300

    Correção

--- trab/main.c
+++ trab/main.c
@@ -4,122 +4,108 @@
 #define true 1
 #define false 0
 
-void rgraph(FILE *input, int n, char tabela[n][n]);
-void create_adj( int n, char tabela[n][n], int m_adj[n][n]);
-int n_familias(int n, int m_adj[n][n], int *visitado);
-void n_familias2(int n, int m_adj[n][n], int *visitado, int vertice, int n_familias);
-void conta_graus(int n, int m_adj[n][n], int *graus);
-void conta_vertices(int n, int *visitado, int numero_familias, int *total);
-void verifica (int n, int numero_familias, int *total_familias, int *graus, int *visitado);
+void rgraph(FILE *input, int n, char matriz[n][n]);
+int n_familias(int n, char matriz[n][n], int *visitados);
+void n_familias2(int n, char matriz[n][n], int *visitados, int vertice, int n_familias);
+void conta_graus(int n, char matriz[n][n], int *graus);
+void conta_vertices(int n, int *visitados, int numero_familias, int *total);
+void verifica (int n, int numero_familias, int *total_familias, int *graus, int *visitados);
 
 int main(){
 
     int n;
-    FILE *input = fopen("input/entrada1.txt","r");
+    FILE *input = fopen("input/entradas_novas/11.txt","r");
     fscanf(input,"%d\n",&n);
     
-    char tabela[n][n];
-    int visitado[n];
-    int matriz_adj[n][n];
+    char matriz[n][n];
+    int visitados[n];
     int numero_familias;
     int graus[n];
 
-    rgraph(input, n, tabela);
+    rgraph(input, n, matriz);
     fclose(input);
-    create_adj(n, tabela, matriz_adj);
-    conta_graus(n, matriz_adj, graus);
+    conta_graus(n, matriz, graus);
 
-    numero_familias = n_familias(n, matriz_adj, visitado);
+    numero_familias = n_familias(n, matriz, visitados);
     int total_familias[numero_familias+1];
 
-    conta_vertices(n, visitado, numero_familias, total_familias);
-    verifica(n, numero_familias, total_familias, graus, visitado);
+    conta_vertices(n, visitados, numero_familias, total_familias);
+    verifica(n, numero_familias, total_familias, graus, visitados);
 
     return 0;
 }
 
-void rgraph(FILE *input, int n, char tabela[n][n]) {
+void rgraph(FILE *input, int n, char matriz[n][n]) {
     for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++)
-            fscanf(input, "%c", &tabela[i][j]);
+        for(int j=0; j<n; j++) {
+            fscanf(input, "%c", &matriz[i][j]);
+            if ( i==j && matriz[i][j] == 'F') {
+                printf("INFELIZMENTE OS C(K)AIOS ESTAVAM BEBADOS\n");
+                exit(1);
+            }
+        }
         fscanf(input,"\n");
     }
 }
 
-void create_adj( int n, char tabela[n][n], int m_adj[n][n]) {
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++) {
-            if (i==j)
-                if(tabela[i][j]=='F'){
-                    printf("INFELIZMENTE OS C(K)AIOS ESTAVAM BEBADOS");
-                    exit(1);
-                }
-            if ( tabela[i][j] == 'V')            
-                m_adj[i][j]=1;
-            else   
-                m_adj[i][j]=0;
-        }
-}
-
-int n_familias(int n, int m_adj[n][n], int *visitado) {
+int n_familias(int n, char matriz[n][n], int *visitados) {
     int n_familias=0;
     int i;
     for (i=0; i<n; i++)
-        visitado[i]=-1;
+        visitados[i]=-1;
     for (i=0; i<n; i++)
-        if(visitado[i]==-1) {
+        if(visitados[i]==-1) {
             n_familias++;
-            n_familias2(n, m_adj, visitado, i, n_familias);
+            n_familias2(n, matriz, visitados, i, n_familias);
         }
     return n_familias;
 }
 
-void n_familias2(int n, int m_adj[n][n], int *visitado, int vertice, int n_familias) {
-    visitado[vertice] = n_familias;
+void n_familias2(int n, char matriz[n][n], int *visitados, int vertice, int n_familias) {
+    visitados[vertice] = n_familias;
     for(int j=0; j<n; j++)
-        if ( m_adj[vertice][j] == 1)
-            if (visitado[j] == -1)
-                n_familias2(n, m_adj, visitado, j, n_familias);
+        if ( matriz[vertice][j] == 'V' && visitados[j] == -1)
+                n_familias2(n, matriz, visitados, j, n_familias);
 }
 
-void conta_graus(int n, int m_adj[n][n], int *graus) {
+void conta_graus(int n, char matriz[n][n], int *graus) {
     int i, j;
     int contador;
     for (i=0; i<n; i++) {
         contador=0;
         for(j=0; j<n; j++)
-            if (m_adj[i][j] != 0 && i!=j) 
+            if (matriz[i][j] == 'V' && i!=j) 
                 contador++;
         graus[i]=contador;
     }
 }
 
-void conta_vertices(int n, int *visitado, int numero_familias, int *total) {
+void conta_vertices(int n, int *visitados, int numero_familias, int *total) {
     int contador;
     for(int i=1; i<=numero_familias; i++) {
         contador=0;
         for(int j=0; j<n; j++)
-            if(visitado[j] == i)
+            if(visitados[j] == i)
                 contador++;
         total[i] = contador;
     }
 }
 
-void verifica (int n, int numero_familias, int *total_familias, int *graus, int *visitado) {
-    int grau_max;
-    int verdadeiro;
-    int i, j;
+void verifica (int n, int numero_familias, int *total_familias, int *graus, int *visitados) {
+    int grau_max, completos=true, i,j;
     for(i=1; i<=numero_familias; i++) {
+        if (!completos)
+            break;
         grau_max = total_familias[i]-1;
         for (j=0; j<n; j++)
-            if ( visitado[j] == i )
+            if ( visitados[j] == i )
                 if ( graus[j] != grau_max ) {
-                    verdadeiro = false;
+                    completos = false;
                     break;
                 } else
-                    verdadeiro = true;  
+                    completos = true;  
     }
-    if (verdadeiro) {
+    if (completos) {
         printf("%d\n", numero_familias);
         for(int i=1;i<=numero_familias;i++) 
             printf("%d ", total_familias[i]);
